4c366ff26b8a3172115353a2db334a2c6042b0ee
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-18 20:52:10 -0300

    Conectando com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+require('express-async-errors')
+const database = require('./database/sqlite')
+const AppError = require('./utils/AppError')
+
 const express = require('express')
 
 const routes = require('./routes')
@@ -7,7 +11,26 @@ app.use(express.json())
 
 app.use(routes)
 
+database()
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+
+  console.error(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
+
 const PORT = 3000
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`)
 })
