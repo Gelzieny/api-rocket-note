435a1a9d52c6d8e1aad5b5d1577532c1440e636d
Gelzieny R. Martins <gelzieny@gmail.com>
2024-12-19 13:15:41 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 require('express-async-errors')
-// const database = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError')
 
 const express = require('express')
@@ -9,8 +8,6 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
-
-// database()
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
